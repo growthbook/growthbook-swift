4cd485947a49c1c6221650a9272438ebe0147c09
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2023-01-10 10:56:36 +0200

    update versions iOS simulator

--- Scripts/runTest.sh
+++ Scripts/runTest.sh
@@ -3,9 +3,9 @@
 xcodebuild -project GrowthBook-IOS.xcodeproj \
    -scheme GrowthBook \
    -sdk iphonesimulator \
-   -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.2'
+   -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0'
 
 xcodebuild test -project GrowthBook-IOS.xcodeproj \
    -scheme GrowthBookTests \
    -sdk iphonesimulator \
-   -destination 'platform=iOS Simulator,name=iPhone 13,OS=15.2'
+   -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0'
