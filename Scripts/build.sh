dbd6f342e1a5b7053d2e365450ce2e7e718ef95d
Logan Blevins <logan.blevins@angel.com>
2024-03-14 20:43:21 -0500

    fix(build): Comment and revert script changes

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
 
+## Currently a bug introduced in Xcode 15.2 unresolved where the visionOs destination will use the iOS base SDK.
+## Workaround available here: https://developer.apple.com/documentation/xcode-release-notes/xcode-15_2-release-notes
+## This build script will fail unless workaround is applied.
+
 xcodebuild archive \
     -scheme GrowthBook \
     -archivePath "./build/ios.xcarchive" \
     -destination "generic/platform=iOS" \
+    SKIP_INSTALL=NO \
+
+xcodebuild archive \
+    -scheme GrowthBook \
+    -archivePath "./build/ios_sim.xcarchive" \
     -destination "generic/platform=iOS Simulator" \
-    SKIP_INSTALL=NO
+    SKIP_INSTALL=NO \
 
 xcodebuild archive \
     -scheme GrowthBook \
     -archivePath "./build/tv.xcarchive" \
     -destination "generic/platform=tvOS" \
+    SKIP_INSTALL=NO \
+
+xcodebuild archive \
+    -scheme GrowthBook \
+    -archivePath "./build/tv_sim.xcarchive" \
     -destination "generic/platform=tvOS Simulator" \
-    SKIP_INSTALL=NO
+    SKIP_INSTALL=NO \
 
 xcodebuild archive \
     -scheme GrowthBook \
     -archivePath "./build/watch.xcarchive" \
     -destination "generic/platform=watchOS" \
+    SKIP_INSTALL=NO \
+
+xcodebuild archive \
+    -scheme GrowthBook \
+    -archivePath "./build/watch_sim.xcarchive" \
     -destination "generic/platform=watchOS Simulator" \
-    SKIP_INSTALL=NO
+    SKIP_INSTALL=NO \
 
 xcodebuild archive \
     -scheme GrowthBook \
     -archivePath "./build/vision.xcarchive" \
     -destination "generic/platform=visionOS" \
+    SKIP_INSTALL=NO \
+
+xcodebuild archive \
+    -scheme GrowthBook \
+    -archivePath "./build/vision_sim.xcarchive" \
     -destination "generic/platform=visionOS Simulator" \
-    SKIP_INSTALL=NO
+    SKIP_INSTALL=NO \
 
 xcodebuild archive \
     -scheme GrowthBook \
@@ -36,8 +60,12 @@ xcodebuild archive \
 
 xcodebuild -create-xcframework \
     -framework "./build/ios.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
+    -framework "./build/ios_sim.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
     -framework "./build/tv.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
+    -framework "./build/tv_sim.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
     -framework "./build/watch.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
+    -framework "./build/watch_sim.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
     -framework "./build/vision.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
+    -framework "./build/vision_sim.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
     -framework "./build/macos.xcarchive/Products/Library/Frameworks/GrowthBook.framework" \
     -output "./build/GrowthBook.xcframework"
