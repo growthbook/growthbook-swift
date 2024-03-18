5d06e772ed0c73df24f4cb3f872fc04399f0047a
Logan Blevins <logan.blevins@angel.com>
2024-03-18 15:51:16 -0500

    tests: Add missing platforms

--- Scripts/runTest.sh
+++ Scripts/runTest.sh
@@ -3,3 +3,15 @@
 xcodebuild test -project GrowthBook-IOS.xcodeproj \
    -scheme GrowthBook \
    -destination 'platform=iOS Simulator,name=iPhone 15'
+
+xcodebuild test -project GrowthBook-IOS.xcodeproj \
+   -scheme GrowthBook \
+   -destination 'platform=tvOS Simulator,name=Apple TV 4K (3rd generation) (at 1080p)'
+
+xcodebuild test -project GrowthBook-IOS.xcodeproj \
+   -scheme GrowthBook \
+   -destination 'platform=watchOS Simulator,name=Apple Watch Series 9 (45mm)'
+
+xcodebuild test -project GrowthBook-IOS.xcodeproj \
+   -scheme GrowthBook \
+   -destination 'platform=visionOS Simulator,name=Apple Vision Pro'
