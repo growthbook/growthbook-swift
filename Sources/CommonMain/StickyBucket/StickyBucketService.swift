13a952191f416bf3ca7c24615b39452a24a55a88
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2025-08-12 16:58:58 +0300

    update

--- Sources/CommonMain/StickyBucket/StickyBucketService.swift
+++ Sources/CommonMain/StickyBucket/StickyBucketService.swift
@@ -8,21 +8,17 @@ import Foundation
 
 @objc public class StickyBucketService: NSObject, StickyBucketServiceProtocol {
     private let prefix: String
-    private let localStorage: CachingLayer?
+    private let localStorage = CachingManager()
     
-    public init(prefix: String = "gbStickyBuckets__", localStorage: CachingLayer? = CachingManager()) {
+    public init(prefix: String = "gbStickyBuckets__", localStoragePath: CacheDirectory = .applicationSupport) {
         self.prefix = prefix
-        self.localStorage = localStorage
+        localStorage.setSystemCacheDirectory(localStoragePath)
     }
     
     public func getAssignments(attributeName: String, attributeValue: String) -> StickyAssignmentsDocument? {
         let key = "\(attributeName)||\(attributeValue)"
-        
-        guard let localStorage = localStorage else { return nil }
-                
-        if
-            let data = localStorage.getContent(fileName: prefix + key),
-            let jsonPetitions = try? JSONDecoder().decode(StickyAssignmentsDocument.self, from: data) {
+                        
+        if let data = localStorage.getContent(fileName: prefix + key), let jsonPetitions = try? JSONDecoder().decode(StickyAssignmentsDocument.self, from: data) {
             return jsonPetitions
         }
 
@@ -32,7 +28,7 @@ import Foundation
     public func saveAssignments(doc: StickyAssignmentsDocument) {
         let key = "\(doc.attributeName)||\(doc.attributeValue)"
         
-        guard let localStorage = localStorage, let docData = try? JSONEncoder().encode(doc) else { return }
+        guard let docData = try? JSONEncoder().encode(doc) else { return }
 
         localStorage.saveContent(fileName: prefix + key, content: docData)
     }
