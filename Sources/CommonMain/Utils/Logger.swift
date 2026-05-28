44161427cf51a59def4080662d0f2491baa5b3b1
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsolutions.com>
2026-05-28 16:51:51 +0300

    fix: Swift 5.9 compatibility

--- Sources/CommonMain/Utils/Logger.swift
+++ Sources/CommonMain/Utils/Logger.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 // GrowthBook default logger
-nonisolated(unsafe) var logger = GBLogger()
+var logger = GBLogger()
 
 @objc public enum LoggerLevel: NSInteger {
     case trace = 0
